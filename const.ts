6e7ecf9d270c483ec60f2d570bb15abb05a7e81a
andresfel22 <andres.sierra@indtechcol.com>
2025-11-03 13:17:42 -0500

    rules vercel

--- const.ts
+++ const.ts
@@ -1,2 +1,2 @@
-export const URLAPI = 'http://69.62.66.124:5000/' //"http://localhost:5000/";
-export const URLLOCAL = "https://crmweb-beta.vercel.app/" // "http://localhost:5173/";
+export const URLAPI = "http://69.62.66.124:5000/"; //"http://localhost:5000/";
+export const URLLOCAL = "https://crmweb-beta.vercel.app/"; // "http://localhost:5173/";
